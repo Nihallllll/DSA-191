f7b55009551bc352104fa0ea763f1ba3bb28f1ad
Nihallllll <nihal24rajak@gmail.com>
2026-04-26 22:18:17 +0530

    kadane

---
+++ 5.kadane.cpp
@@ -0,0 +1,15 @@
+//Final appraoch since i remeber it how to solve
+#include<vector>
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum =INT_MIN;
+        int sum =0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            maxSum =max(maxSum , sum);
+            if(sum<0){sum = 0;}
+        }
+        return maxSum;
+    }
+};
