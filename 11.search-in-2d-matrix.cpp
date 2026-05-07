964b16339852ac29c96190e6f7595b124d53ed95
Nihallllll <nihal24rajak@gmail.com>
2026-05-07 20:44:11 +0530

    search-in-2d-matrix

---
+++ 11.search-in-2d-matrix.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int low = 0;
+        int high = row * col - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int r = mid / col;
+            int c = mid % col;
+
+            if (matrix[r][c] == target) {
+                return true;
+            }
+
+            else if (matrix[r][c] < target) {
+                low = mid + 1; // GO RIGHT
+            }
+
+            else {
+                high = mid - 1; // GO LEFT
+            }
+        }
+        return false;
+    }
+};
